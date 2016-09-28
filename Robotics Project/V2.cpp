1cf19713096d1a09f599a04ab6fecfee07432ad9
izaaksulka <izaaksulka@mines.edu>
2016-09-28 14:41:32 -0600

    Implemented a few functions in V2

---
+++ Robotics Project/V2.cpp
@@ -0,0 +1,33 @@
+#include "V2.h"
+
+//using namespace std;
+
+
+V2::V2() {
+	x = 0;
+	y = 0;
+}
+V2::V2(int newX, int newY) {
+	x = newX;
+	y = newY;
+}
+
+int V2::GetX() {
+	return x;
+}
+int V2::GetY() {
+	return y;
+}
+void V2::SetX(int newX) {
+	x = newX;
+}
+void V2::SetY(int newY) {
+	y = newY;
+}
+
+V2 V2::operator+(V2 other) {
+	return V2(x + other.GetX(), y + other.GetY());
+}
+V2 V2::operator-(V2 other) {
+	return V2(x - other.GetX(), y - other.GetY());
+}
--- Robotics Project/V2.h
+++ Robotics Project/V2.h
@@ -1,10 +1,20 @@
 #pragma once
 class V2 {
 public:
+	V2();
+	V2(int newX, int newY);
 
+	int GetX();
+	int GetY();
+	void SetX(int newX);
+	void SetY(int newY);
 
+	V2 operator+(V2 other);
+	V2 operator-(V2 other);
 
+	void Print();
 private:
+	int x;
+	int y;
 
-
-}
+};
