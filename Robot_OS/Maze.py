6fd42013298f8803b3bd292c6adf1f212f84a891
Easton Bornemeier <Easton Bornemeier>
2017-03-28 15:29:43 -0600

    cleaned up and made GetEnds return ends, need to figure out what to do with ends before returning. (Sorting?)

--- Robot_OS/Maze.py
+++ Robot_OS/Maze.py
@@ -27,6 +27,7 @@ class Maze:
         
         #self.lastPosition = Vector.Vector(0, 6)
 
+        #Set the starting tile to START state
         self.tiles[0][6].SetState(0)
         #self.Output( 0, 6 )  
         self.PrintMap()
@@ -40,22 +41,18 @@ class Maze:
             self.PrintMap()
         
     def Output(self, x, y):
+        
+        #make sure it is within the bounds to write
         if x >= 0 and x < self.width and y >= 0 and y < self.height:
+        	#create the output string
             output = "-1 %d %d %d\n" % ( y, x, self.tiles[x][y].GetColor() )
-            #print( "To Map: ", output ) 
+            #write to the serial 
             self.ser.write( output.encode( encoding = "ascii" ) )
         else:
             print( "%d %d %d is out of range!" % ( y, x, self.tiles[x][y].GetColor() ) )        
-        '''
-        if(position != self.lastPosition):
-            self.tiles[lastx][lasty].UpdateACSensorData(False)
-            self.lastPosition = position
         
-        
-        lastx = int(self.lastPosition[0])
-        lasty = int(self.lastPosition[1])
-        '''
-    def PrintMap(self):
+
+        def PrintMap(self):
         output = ""
         for i in range( self.width ):
             for j in range( self.height ):
@@ -64,55 +61,64 @@ class Maze:
 
         #output += "\n"
         self.ser.write( output.encode( encoding = "ascii" ) )
+
+
     def GetEnds(self):
         ends = self.Connect()
+
         if len(ends) < 2:
             print( "Oh god something's wrong.  there's less than 2 end points")
             #do something to handle that there are not enough end points
+
         elif len(ends) == 2:#meaning there's no corners here
             for end in ends:
                 end.SetState(WIRE) #2 is WIRE, trying importing from Tile at top
+            PrintMap()
+
         else:#meaning that there are more than two ends, so there is a corner
             print("need to handle corners")
-            
+            #wait to set LED until gotten to the corner
+
         return ends
+
     def Connect(self):
+
         ends = []
+
         #for every tile 1 space from the edge
         for i in range(1, 6):
-            #top row
+            
             if (self.tiles[i][1].IsWire()):#Across the top
                 if GetAdjWireCount(i, 1) == 1:
                     ends.append(i, 0)
+
             if (self.tiles[i][5].IsWire()):#Across the bottom
                 if GetAdjWireCount(i, 5) == 1:
                     ends.append(i, 6)
+
             if (self.tiles[1][i].IsWire()):#Down the left side
                 if GetAdjWireCount(1, i) == 1:
                     ends.append(0, i)
+
             if (self.tiles[5][i].IsWire()):#Down the right side
                 if GetAdjWireCount(5, i) == 1:
                     ends.append(6, i)
-        if(len(ends) == 2):
-            return ends
-        #if the length of ends was not equal to 2 then have to figure out what to do next
-        print("found some corners")
+
         return ends
-       # print( "Connect ends found: ", len(ends) )
-       # print( "Hello world!" )    
-        
+
     def GetAdjTiles(self, pos):
         adjs = []
-        if pos[0] > 0:#left
+        if pos[0] == 0:#left
             adjs.append(pos[0] - 1, pos[1])
-        if pos[0] < width - 1:#right
+        if pos[0] < width - 2:#right
             adjs.append(pos[0] + 1, pos[1])
         if pos[1] > 0:#up
             adjs.append(pos[0],     pos[0] - 1)
-        if pos[1] < height - 1:#down
+        if pos[1] < height - 2:#down
             adjs.append(pos[0],     pos[0] + 1)
         return adjs
     def GetAdjWireCount(self, pos):
+
         adjs = GetAdjTiles(pos)
         i = 0
         for t in adjs:
